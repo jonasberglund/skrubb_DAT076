9bbc67b10abd1b070b2516f249872a50347a655a
roffin <robin.tornquist@gmail.com>
2013-10-07 13:39:46 +0200

    back end init

---
+++ blog_back_end/src/main/java/com/skrubb/blog_back_end/App.java
@@ -0,0 +1,13 @@
+package com.skrubb.blog_back_end;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

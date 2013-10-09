e33409fa5e2b84d18649185c9f4312a18db66c0e
jonasberglund <jonasberglund@dhcp-187057.eduroam.chalmers.se>
2013-10-09 14:52:58 +0200

    added image post

---
+++ blog_front_end/src/main/java/com/skrubb/blog_front_end/FileUploadController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.skrubb.blog_front_end;
+
+import javax.faces.application.FacesMessage;  
+import javax.faces.context.FacesContext;  
+  
+import org.primefaces.event.FileUploadEvent;  
+import org.primefaces.model.UploadedFile;  
+  
+public class FileUploadController {  
+  
+    public void handleFileUpload(FileUploadEvent event) {  
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }  
+}  

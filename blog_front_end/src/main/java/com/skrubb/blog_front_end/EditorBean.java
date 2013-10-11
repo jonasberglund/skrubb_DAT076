cb231431b9bdf98889252917c47f30ffa03b9987
vispen <anders.ma.johansson@gmail.com>
2013-10-11 17:49:08 +0200

    added titel in post started cooment

--- blog_front_end/src/main/java/com/skrubb/blog_front_end/EditorBean.java
+++ blog_front_end/src/main/java/com/skrubb/blog_front_end/EditorBean.java
@@ -17,7 +17,8 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class EditorBean {
     
-    private String value;  
+    private String value;
+    private String title;
   
     public String getValue() {  
         return value;  
@@ -26,9 +27,21 @@ public class EditorBean {
     public void setValue(String value) {  
         this.value = value;  
         
-    } 
+    }
+    public String getTitle() {  
+        return title;  
+    }  
+  
+    public void setTitle(String title) {  
+        this.title = title;  
+        
+    }
     public void textPost(){
     
-        DummyDB.listOfPost.add(new TextPost(DummyDB.a1, null, "titel1", null, getValue()));
-    } 
+        DummyDB.listOfPost.add(new TextPost(DummyDB.a1, null, getTitle(), null, getValue()));
+    }
+    public void textClear(){
+    
+        this.value="";
+    }
  }
